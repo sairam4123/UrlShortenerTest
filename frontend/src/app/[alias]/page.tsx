817b0c01d686b90f96d8fbdaea53ed8a809eff43
Sairam M <sairam.m.2005.65@gmail.com>
2025-12-26 01:21:02 +0530

    Add SimpleIcon component to RedirectPage for enhanced branding and user engagement

--- frontend/src/app/[alias]/page.tsx
+++ frontend/src/app/[alias]/page.tsx
@@ -4,6 +4,7 @@ import { useRouter, useParams } from "next/navigation";
 import { api } from "@/api/api";
 import { motion } from "framer-motion";
 import { Spinner } from "@/@components/Spinner";
+import { SimpleIcon } from "@/@components/SimpleIcons";
 
 export default function RedirectPage() {
   const router = useRouter();
@@ -36,8 +37,39 @@ export default function RedirectPage() {
         transition={{ duration: 0.4 }}
       >
         <Spinner size="lg" color="blue" />
-        <p className="text-lg text-neutral-300">Redirecting...</p>
+        <p className="text-lg mt-2 text-neutral-300">Redirecting...</p>
+
+        {longUrl && (
+          <p className="text-center text-neutral-400 max-w-md">
+            If you are not redirected automatically,
+            <a
+              href={longUrl || (error as Error)?.cause || "/"}
+              className="text-blue-400 underline ml-1"
+            >
+              click here
+            </a>
+            .
+          </p>
+        )}
       </motion.div>
+
+      <p className="absolute bottom-10 text-center flex items-center justify-center flex-row gap-2 font-light text-neutral-300 text-md">
+        Made with ❤️ by
+        <span className="font-semibold bg-gradient-to-r from-blue-500 to-blue-300 text-transparent bg-clip-text">
+          Sairam Mangeshkar
+        </span>
+        <SimpleIcon
+          name="siGithub"
+          className="mx-auto h-5 w-5 text-neutral-400 hover:text-neutral-200 transition cursor-pointer"
+          onClick={() =>
+            window.open("https://github.com/sairam4123/UrlShortenerTest")
+          }
+        />
+      </p>
+
+      <p className="absolute bottom-2 text-center font-light text-neutral-400 text-xs ">
+        © 2025. All rights reserved.
+      </p>
     </div>
   );
 }
